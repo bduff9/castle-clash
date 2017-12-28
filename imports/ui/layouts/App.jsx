ccd3f1b54a61cb9dc1ff473ff7cc29e611bdffc8
Brian Duffey <bduffey2@BrianDufeysiMac.lan>
2017-12-27 22:11:36 -0600

    Added styled components

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -2,25 +2,43 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
+import styled from 'styled-components';
 
 import { authenticatedType, loggingInType, pageReadyType, userType, userIDType } from '../helpers/types';
-//import { Routes } from '../../startup/client/Routes.jsx';
+import Routes from '../../startup/client/Routes.jsx';
 import Loading from '../components/Loading';
 
-const App = ({ authenticated, loggingIn, pageReady, userID }) => {
+const AppGrid = styled.div`
+	display: grid;
+	grid-template-rows: minmax(50px, auto) 1fr;
+	grid-template-columns: minmax(200px, auto) 1fr;
+	grid-template-areas:
+		"header header"
+		"sidebar maincontent";
+`;
+
+const Header = styled.header`
+	grid-area: header;
+`;
+
+const SideBar = styled.section`
+	grid-area: sidebar;
+`;
+
+const App = ({ authenticated, loggingIn, pageReady, user, userID }) => {
 	return (
-		<div className="row">
+		<AppGrid id="app">
 			<Helmet
 				htmlAttributes={{ lang: 'en', 'amp': undefined }}
 				title="Welcome"
 				titleTemplate="%s | Castle Clash Tracker"
 				link={[{ rel: 'icon', sizes: '16x16 32x32', href: '/favicon.png?v=1' }]}
 				meta={[{ 'charset': 'utf-8' }, { 'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge' }, { 'name': 'viewport', 'content': 'width=device-width, initial-scale=1, user-scalable=no' }]} />
-			{pageReady ? 'Ready'
-			//<Routes authenticated={authenticated} loggingIn={loggingIn} key={`current-user-${userID}`} />
-				: <Loading />
+			<Header user={user}>Header</Header>
+			<SideBar authenticated={authenticated}>Side Bar</SideBar>
+			{pageReady ? <Routes authenticated={authenticated} loggingIn={loggingIn} key={`current-user-${userID}`} /> : <Loading />
 			}
-		</div>
+		</AppGrid>
 	);
 };
 
